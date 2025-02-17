b78f29455bc0877ac6cde2127147ed1bb535e7b7
ravichandrapalla <cravi6635@gmail.com>
2025-02-17 20:39:10 +0530

    till basic layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Roboto, Lusitana} from "next/font/google";
+
+export const inter = Roboto({subsets: ["latin"], weight: "300"});
+
+export const lusitana = Lusitana({weight: ["400", "700"], subsets:["latin"]});
